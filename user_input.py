0137682b2af5b34b34672d857c8925d08eced0d0
Nekaze <jnkoziuk@gmail.com>
2020-11-29 15:22:26 -0300

    Added user_input

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name      = input("Nombre: ")
+adjective = input("Como es? ")
+material  = input("De que material es? ")
+carita    = input("Como se lava la carita? ")
+
+print("----------------------------------------")
+print(name + " es un muñeco " + adjective + " y de " + material)
+print("se lava la carita " + carita)
