2e5d8bb16d4f093bbb51a63de57a08102f5386c1
Nekaze <jnkoziuk@gmail.com>
2020-11-29 15:12:22 -0300

    Added variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+
+print ("Vamos sigamos ya")
+print ("continuemos con los sueños a lograr")
+print ("ya la puerta se abrirá")
+print ("en el cielo y las estrellas brillarán")
+print ("la oportunidad comenzará")
+print ("desafiando a los dioses")
+
+print ("Vuela pega y esquiva")
+print ("vamos, hazlo, deja el miedo,")
+print ("se fuerte, nunca perderás,")
+print ("en un héroe te convertirás,")
+print ("no lo dudes, las barreras romperás.")
+
+print ("Vuela, pega y esquiva")
+print ("vamos, hazlo con más fuerza")
+print ("tiemblo de la emoción,")
+print ("lo increíble pronto nos sorprenderá.")
+
